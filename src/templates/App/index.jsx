c5eb9aa41acec00097a7d567c0130235cb2170c0
André Nicolas Cordeiro <andrenicolascordeiro@gmail.com>
2023-02-24 19:14:35 -0300

    ThemeProviderStyled

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,19 +1,25 @@
 import { PostsProvider } from '../../contexts/PostsProvider';
+import { ThemeProvider } from 'styled-components';
 import { CounterProvider } from '../../contexts/CounterProvider';
+import { theme } from '../../styles/theme';
 import * as S from './styles';
 import { Heading } from '../../components/Heading';
+import { GlobalStyles } from '../../styles/global-styles';
 
 export function App() {
   return (
     <CounterProvider>
-      <PostsProvider>
-        <S.Wrapper background="blue">
-          <div>
-            <h1>Hello world!</h1>
-            <Heading>O texto teste</Heading>
-          </div>
-        </S.Wrapper>
-      </PostsProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <PostsProvider>
+          <S.Wrapper background="blue">
+            <div>
+              <h1>Hello world!</h1>
+              <Heading>O texto teste</Heading>
+            </div>
+          </S.Wrapper>
+        </PostsProvider>
+      </ThemeProvider>
     </CounterProvider>
   );
 }
